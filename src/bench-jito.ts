e87ca8f4e4fc5b3eb35710f52551bb029ca27030
bloxflux <developer@bloxflux.com>
2025-02-09 21:14:00 +0800

    feat: 增加耗时统计

--- src/bench-jito.ts
+++ src/bench-jito.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 import "dotenv/config";
 import { logger } from "./logger";
 
-const jitoUrl = process.env.JITO_URL || "";
+const jitoUrl = process.env.JITO_URL || "https://amsterdam.mainnet.block-engine.jito.wtf";
 const concurrency = parseInt(process.env.BENCH_CONCURRENCY || "10", 10);
+logger.info(`bench jito url: ${jitoUrl}`);
 logger.info(`bench concurrency: ${concurrency}/s`);
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -11,6 +12,7 @@ const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 // 新增统计变量
 let jitoRequestCount = 0;
 let error429Count = 0;
+let totalRequestCount = 0;
 let lastLogTime = Date.now();
 
 // 每10秒输出统计信息
@@ -19,10 +21,11 @@ function logStatistics() {
   if (now - lastLogTime >= 10000) {
     // 10秒
     logger.warn(
-      `统计 - 每10秒Jito成功请求量: ${jitoRequestCount}, 平均每秒: ${
+      `统计 - 每10秒发送请求总量: ${totalRequestCount}, 成功请求量: ${jitoRequestCount}, 平均每秒: ${
         jitoRequestCount / 10
       }, 429错误次数: ${error429Count}`
     );
+    totalRequestCount = 0;
     jitoRequestCount = 0;
     error429Count = 0;
     lastLogTime = now;
@@ -39,28 +42,24 @@ async function run() {
     params: [],
   };
 
-  const bundle_resp = await axios.post(`${jitoUrl}/api/v1/bundles`, bundle, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    // httpsAgent: agent, // 使用自定义 agent
-    // localAddress: selectedIp, // 指定源IP地址
-  } as any);
-  if (bundle_resp.status === 200) {
-    jitoRequestCount++; // 成功请求计数
-    logger.info(`200: 发送成功`);
-  } else {
-    logger.error(`发送失败`);
+  try {
+    const bundle_resp = await axios.post(`${jitoUrl}/api/v1/bundles`, bundle, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (bundle_resp.status === 200) {
+      jitoRequestCount++; // 成功请求计数
+      const duration = Date.now() - start;
+      logger.info(`200: 发送成功, 耗时: ${duration}ms`);
+    } else {
+      logger.error(`发送失败`);
+    }
+  } catch (error) {
+    const duration = Date.now() - start;
+    logger.error(`请求失败, 耗时: ${duration}ms, 错误: ${error.message}`);
   }
-
-  // const tipAccounts = bundle_resp.data.result;
-  // logger.info(`tipAccounts: ${tipAccounts}`);
-
-  // cal time cost
-  const end = Date.now();
-  const duration = end - start;
-
-  // logger.info(`total duration: ${duration}ms`);
 }
 
 async function main() {
@@ -78,6 +77,7 @@ async function main() {
         })
       );
 
+    totalRequestCount += concurrency; // 统计总请求量
     await Promise.all(promises);
 
     // 输出统计信息
