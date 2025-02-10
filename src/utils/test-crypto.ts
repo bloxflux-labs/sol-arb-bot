14dbea767162eff908ad703a2c70048465515c09
bloxflux <developer@bloxflux.com>
2025-02-10 19:55:04 +0800

    chore: test crypto

--- src/utils/test-crypto.ts
+++ src/utils/test-crypto.ts
@@ -4,6 +4,7 @@ import { decrypt, encrypt } from "./cryptoUtils";
 
 // 测试加密和解密
 const originalKey = process.env.PRIVATE_KEY;
+// const originalKey = "";
 if (!originalKey) {
   throw new Error("PRIVATE_KEY is not defined in environment variables");
 }
@@ -11,13 +12,13 @@ const encryptedKey = encrypt(originalKey);
 console.log("Encrypted Key:", encryptedKey);
 
 // 从环境变量中获取加密后的私钥并解密
-const encryptedPrivateKey = process.env.ENCRYPTED_PRIVATE_KEY;
-if (!encryptedPrivateKey) {
-  throw new Error("ENCRYPTED_PRIVATE_KEY is not defined in environment variables");
-}
-console.log("Encrypted Private Key:", encryptedPrivateKey);
+// const encryptedPrivateKey = process.env.ENCRYPTED_PRIVATE_KEY;
+// if (!encryptedPrivateKey) {
+//   throw new Error("ENCRYPTED_PRIVATE_KEY is not defined in environment variables");
+// }
+// console.log("Encrypted Private Key:", encryptedPrivateKey);
 
-const decryptedKey = decrypt(encryptedPrivateKey);
+const decryptedKey = decrypt(encryptedKey);
 // console.log("Decrypted Key:", decryptedKey);
 
 const payer = Keypair.fromSecretKey(bs58.decode(decryptedKey));
